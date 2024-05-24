2ffc988f6b3de9381d4073e5eab201e75ebf215e
Tim-Alpha <codewithaisha@gmail.com>
2024-05-25 02:39:45 +0530

    update: add sharp & path libirary

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const cors = require('cors')
+const sharp = require('sharp');
+const path = require('path');
 
 const app = express();
 const port = 3000;
